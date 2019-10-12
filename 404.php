70a844c09848395c553fe93d415f451d56fe4bcd
Matthew Pritchett <matthew.r.pritchett@gmail.com>
2019-10-12 11:27:38 -0400

    Updates 404 page to match theme structure and style.
    Fixes #1

--- 404.php
+++ 404.php
@@ -10,45 +10,18 @@
 get_header();
 ?>
 
-	<div id="primary" class="wrapper content-area">
-		<main id="main" class="site-main">
+	<div id="primary" class="wrapper content-area flex fd-c jc-c">
+		<main id="main" class="site-main ta-c">
 
-			<section class="error-404 not-found">
+			<section class="error-404 not-found flex fd-c jc-c">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'builder-blocks-blog' ); ?></h1>
+					<h1 class="page-title"><?php esc_html_e( 'Sorry, page not found', 'builder-blocks-blog' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'builder-blocks-blog' ); ?></p>
+					<p><?php esc_html_e( 'But you can always search for what you\'re looking for.', 'builder-blocks-blog' ); ?></p>
 
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'builder-blocks-blog' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$builder_blocks_blog_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'builder-blocks-blog' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$builder_blocks_blog_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+					<?php get_search_form(); ?>
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
