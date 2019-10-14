bba3889e640d918f39cb710e71bb8c7f0492a273
Matthew Pritchett <matthew.r.pritchett@gmail.com>
2019-10-14 11:45:34 -0400

    Updates to post content style.

--- template-parts/content.php
+++ template-parts/content.php
@@ -15,7 +15,7 @@
 			<div class="entry-title-wrapper">
 				<?php
 					/* translators: used between list items, there is a space after the comma */
-					$categories_list = get_the_category_list( esc_html__( ', ', 'builder-blocks-blog' ) );
+					$categories_list = get_the_category_list( esc_html__( ' ', 'builder-blocks-blog' ) );
 					if ( $categories_list ) {
 						/* translators: 1: list of categories. */
 						printf( '<span class="cat-links">' . esc_html__( '%1$s', 'builder-blocks-blog' ) . '</span>', $categories_list ); // WPCS: XSS OK.
